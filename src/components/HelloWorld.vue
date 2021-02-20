a4f4e6d1bd26920864282f2788076a086debfa09
unknown <sahanbcsrh@gmail.com>
2021-02-20 11:52:32 +0530

    first

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,89 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+ <template>
+  <div>
+    <div class="jumbotron">
+      <h1 class="display-4 text-center">X-Z Solutions</h1>
+    </div>
+
+    <div class="text-center container">
+      <br />
+      <form>
+        <fieldset>
+          <center>
+            <div class="form-group" style="width: 40%">
+              <label for="device"></label>
+              <select id="device" class="form-control">
+                <option>Select Device</option>
+                <option>X</option>
+                <option>Y</option>
+                <option>Z</option>
+              </select>
+            </div>
+          </center>
+          <center>
+            <div class="form-group" style="width: 40%">
+              <label for="devicemodel"></label>
+              <select id="devicemodel" class="form-control">
+                <option>Select Device Model</option>
+                <option>A</option>
+                <option>B</option>
+                <option>c</option>
+              </select>
+            </div>
+          </center>
+          <center>
+            <div class="version" style="width: 40%">
+              <label for="version"></label>
+              <select id="version" class="form-control">
+                <option>Select Version</option>
+                <option>1.0</option>
+                <option>2.0</option>
+                <option>3.0</option>
+              </select>
+            </div>
+          </center>
+          <button
+            type="submit"
+            class="btn btn-primary"
+            style="margin-top: 40px"
+          >
+            Find Best App
+          </button>
+        </fieldset>
+      </form>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="card" style="width: 13rem; margin: 3rem">
+          <img
+            class="card-img-top"
+            src="../assets/1578.png"
+            alt="Card image cap"
+          />
+          <div class="card-body text-center">
+            <h5 class="card-title text-center">App A</h5>
+            <input type="button" class="btn btn-success" value="Install" />
+          </div>
+        </div>
+        <div class="card" style="width: 13rem">
+          <img
+            class="card-img-top"
+            src="../assets/1578.png"
+            alt="Card image cap"
+          />
+          <div class="card-body text-center">
+            <h5 class="card-title text-center">App A</h5>
+            <input type="button" class="btn btn-success" value="Install" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+ 
+ <script>
+export default {};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+ 
+ <style>
 </style>
